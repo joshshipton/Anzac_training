eece2b41a04c934929296ddab84a62c2f7be8825
joshshipton <shiptonjosh@gmail.com>
2023-04-25 18:45:35 +0800

    learnt sieve of e blah blah

--- Divisionals_Spelling.py
+++ Divisionals_Spelling.py
@@ -1,3 +1,20 @@
+# Emilio’s team is competing at the South Pacific ICPC Divisional Finals. They have read the problems and immediately know all the solutions.
+# Emilio finds simply winning the contest boring. Instead, he has convinced
+# his team to spell out a word with their submissions. He has put together a
+# list of n words that he would like to spell.
+# Each question in the contest is labelled with an uppercase letter. The
+# first question is labelled “A”, the second is labelled “B”, and so on. A word
+# is spelled by solving questions in a specific order. For example, if Emilio’s
+# team wants to spell LEAK, the team first solves problem L, then problem E, then problem A, and finally problem
+# K. The team may only solve each problem once, so they may not spell words with duplicated letters (for example,
+# they cannot spell EMILIO). Which words can Emilio’s team spell?
+# Input
+# The first line contains two integers n (1 ≤ n ≤ 100), which is the number of words in Emilio’s list, and m (1 ≤
+# m ≤ 15), which is the number of questions in the contest.
+# The next n lines describe the words. Each of these lines contains a word with at least 1 and at most 15
+# uppercase letters. All the words are distinct.
+
+
 n = input()
 n = n.split()
 num = int(n[0])
@@ -32,8 +49,4 @@ for word in words:
 
 print(count)
 
-
-
-
-
-
+# solved
---
+++ useful algos/Sieve_of_Eratosthenes.py
@@ -0,0 +1,22 @@
+#Fastest way of generating a list of prime numbers in python
+
+def sieve(n):
+    prime = [True for _ in range(n+1)]
+    prime[0],prime[1] = False,False
+    for i in range(2,int(n**0.5)+1):
+        for j in range(2*i, n+1, i):
+            prime[j] = False 
+    return prime
+
+n = int(input('Enter a number: '))
+
+list = sieve(n)
+
+print('Your primes are: ', end="")
+
+for i in range(n+1):
+    if list[i]:
+        print(i, end=" ")
+
+         
+     
